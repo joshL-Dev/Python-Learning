3f3e5b6b1d9c61fad2adec1286acf67cc49c1716
joshL-Dev <70299357+joshL-Dev@users.noreply.github.com>
2022-06-27 13:10:59 -0600

    Create names_list.py

---
+++ names_list.py
@@ -0,0 +1,11 @@
+names = ['James' , 'Jerry', 'Jim']
+print(names[0])
+print(names[1])
+print(names[2])
+
+message = f"Welcome, {names[0]}, to the Grand Illusion. "
+print(message)
+message = f"Welcome, {names[1]}, to the Grand Illusion. "
+print(message)
+message = f"Welcome, {names[2]}, to the Grand Illusion. "
+print(message)
